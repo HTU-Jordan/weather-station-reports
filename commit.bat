6a9bbe5d3decb19ee05e00ffc7fb5c28f7ee6146
Yanal Kashou <yanal.kashou@gmail.com>
2018-02-19 08:47:57 +0200

    automatic update

--- commit.bat
+++ commit.bat
@@ -2,7 +2,9 @@
 cd C:/Users/yanal.kashou/Projects/weather-station-reports
 git checkout dev
 git add .
-git commit -am "made changes"
-git push
+git commit -m "automatic update"
+git remote add origin remote repository https://github.com/HTU-Jordan/weather-station-reports.git
+git remote -v
+git push origin master
 echo Press Enter...
 read
