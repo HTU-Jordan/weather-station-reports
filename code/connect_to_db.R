a15e035e615bd1a69bd307d19e62e3164cbb95f7
Yanal Kashou <yanal.kashou@gmail.com>
2018-02-21 08:41:54 +0200

    automatic update

--- code/connect_to_db.R
+++ code/connect_to_db.R
@@ -11,7 +11,7 @@ library(stringr)
 
 # Create Connection to Database
 connect_to_db <- function() {
-  con <<- odbcDriverConnect(connection=
+  con <- odbcDriverConnect(connection=
                               'dsn=weather-ODBC;
                             driver={SQL Server Native Client 11.0};
                             trusted_connection=yes')
@@ -29,10 +29,10 @@ disconnect_from_db <- function(con) {
 ################################################################################
 
 # Get data with interval 15 Seconds
-fetch_15sec_data <- function(number_of_rows = 5760) {
+fetch_15sec_data <- function(number_of_rows) {
   # Open connection to database
   con <- connect_to_db()
-  q <- str_c("select top ", number_of_rows, " from perFIFTEENSEC") 
+  q <- str_c("select top ", number_of_rows, " * from perFIFTEENSEC order by rec desc") 
   data <- sqlQuery(con,
                    q,
                    stringsAsFactors = FALSE,
@@ -48,10 +48,10 @@ fetch_15sec_data <- function(number_of_rows = 5760) {
 
 
 # Get data with interval 1 Minute
-fetch_1min_data <- function(number_of_rows = 1440) {
+fetch_1min_data <- function(number_of_rows) {
   # Open connection to database
   con <- connect_to_db()
-  q <- str_c("select top ", number_of_rows, " from perMINUTE") 
+  q <- str_c("select top ", number_of_rows, " * from perMINUTE order by rec desc") 
   data <- sqlQuery(con,
                    q,
                    stringsAsFactors = FALSE,
@@ -66,10 +66,10 @@ fetch_1min_data <- function(number_of_rows = 1440) {
 }
 
 # Get data with interval 1 Hour
-fetch_1hour_data <- function(number_of_rows = 168) {
+fetch_1hour_data <- function(number_of_rows) {
   # Open connection to database
   con <- connect_to_db()
-  q <- str_c("select top ", number_of_rows, " from perHOUR") 
+  q <- str_c("select top ", number_of_rows, " * from perHOUR order by rec desc") 
   data <- sqlQuery(con,
                    q,
                    stringsAsFactors = FALSE,
--- code/plot_randomizer.R
+++ code/plot_randomizer.R
@@ -4,4 +4,34 @@
 library(ggplot2)
 library(openair)
 library(scales)
+library(lubridate)
+library(reshape2)
 
+
+temperature_plot <- function(df) {
+  subtract_month <- tail(df$ts, 1) %m-% months(1)
+  df <- subset(df, ts > subtract_month)
+  df$day <- factor(df$ts)
+  t0 <- strptime(df$ts, "%Y-%m-%d %H:%M:%S")
+  df$day <- format(t0, "%Y-%m-%d")
+  df_agg <- aggregate(df$temp, by = list(df$day), function(x) {
+    c(max = max(x), min = min(x)) })
+  
+  xdf <- NULL
+  xdf$day <- df_agg$Group.1
+  xdf$max <- df_agg$x[, 1]
+  xdf$min <- df_agg$x[, 2]
+  xdf$avg <- (xdf$max + xdf$min) / 2
+  xdf <- data.frame(xdf)
+  
+  t <- ggplot(xdf, aes(x = day, y = avg, ymin = min, ymax = max)) +
+    geom_line(aes(y = max), color = "steelblue", size = 1, group = 1) +
+    geom_line(aes(y = min), color = "steelblue", size = 1, group = 1) +
+    geom_pointrange(color = "black", size= 0.75) +
+    geom_point(aes(y = max), color = "steelblue", size = 3.5) +
+    geom_point(aes(y = min), color = "steelblue", size = 3.5) +
+    theme_minimal() +
+    ylab("Air Temperature") +
+    theme(axis.title.x=element_blank())
+  return(t)
+}
